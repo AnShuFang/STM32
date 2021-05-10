897c51d18e62d1d2dad1d4baf04a3a4ae00d9180
AnShu Fang <80732088+AnShuFang@users.noreply.github.com>
2021-05-10 18:51:55 +0800

    Add files via upload

---
+++ BEEP.c
@@ -0,0 +1,28 @@
+#include "stm32h10x.h" 
+#include "sys.h"
+#include "delay.h"
+// 蜂鸣器接在三极管集电极，三极管基极与PB8相连，高电平有效 
+void BEEP_Init(void)
+{
+	RCC->APB2ENR|=1<<3;//GPIOB时钟使能
+	
+	GPIOB->CRH &= oxFFFFFFF0;
+	GPIOB->CRH |= ox00000003; //配置工作模式
+	
+	GPIOB->ODR &= oxFFFFFEFF;//初始输出低电平
+	// GPIOB->BRR|=1<<8;//利用BRR寄存器初始化 
+	//PBout(8)=0;//利用地址映射对应的bit位初始化，PBout是宏定义地址解引用 
+	 
+}
+int main()
+{
+	delay_init(10);//防抖 
+	BEEP_Init();
+	while(1)
+	{
+		GPIOB->BSRR|=1<<8;
+		delay_ms(1000);
+		GPIOB->BSR|=1<<8;
+	}
+	return 0;
+}
