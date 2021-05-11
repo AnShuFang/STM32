b0ccf254a28a2b963fb7dcd61051cb56b3060a8e
AnShu Fang <80732088+AnShuFang@users.noreply.github.com>
2021-05-11 20:16:51 +0800

    Update BEEP.c

--- BEEP.c
+++ BEEP.c
@@ -6,10 +6,10 @@ void BEEP_Init(void)
 {
 	RCC->APB2ENR|=1<<3;//GPIOB时钟使能
 	
-	GPIOB->CRH &= oxFFFFFFF0;
-	GPIOB->CRH |= ox00000003; //配置工作模式
+	GPIOB->CRH &= 0xFFFFFFF0;
+	GPIOB->CRH |= 0x00000003; //配置工作模式
 	
-	GPIOB->ODR &= oxFFFFFEFF;//初始输出低电平
+	GPIOB->ODR &= 0xFFFFFEFF;//初始输出低电平
 	// GPIOB->BRR|=1<<8;//利用BRR寄存器初始化 
 	//PBout(8)=0;//利用地址映射对应的bit位初始化，PBout是宏定义地址解引用 
 	 
@@ -23,6 +23,7 @@ int main()
 		GPIOB->BSRR|=1<<8;
 		delay_ms(1000);
 		GPIOB->BRR|=1<<8;
+		delay_ms(1000);
 	}
 	return 0;
 }
